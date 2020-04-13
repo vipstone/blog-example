80bb93366ea89274f891e35e089bde4bfc1c1f3d
wanglei <295628068@qq.com>
2020-04-13 11:19:17 +0800

    添加新的延迟任务的执行方法

--- blog-example/src/main/java/com/example/DelayTaskExample.java
+++ blog-example/src/main/java/com/example/DelayTaskExample.java
@@ -1,5 +1,9 @@
 package com.example;
 
+import io.netty.util.HashedWheelTimer;
+import io.netty.util.Timeout;
+import io.netty.util.TimerTask;
+
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -27,6 +31,31 @@ public class DelayTaskExample {
         // 2、ScheduledExecutorService 实现延迟任务
         System.out.println("程序启动时间：" + LocalDateTime.now());
         scheduledExecutorServiceTask();
+
+        // 3、Netty
+        System.out.println("程序启动时间：" + LocalDateTime.now());
+        NettyTask();
+    }
+
+    /**
+     * 基于 Netty 的延迟任务
+     */
+    private static void NettyTask() {
+        // 创建延迟任务实例
+        HashedWheelTimer timer = new HashedWheelTimer(3, // 时间间隔
+                TimeUnit.SECONDS,
+                100); // 时间轮中的槽数
+        // 创建一个任务
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run(Timeout timeout) throws Exception {
+                System.out.println("执行任务" +
+                        " ，执行时间：" + LocalDateTime.now());
+            }
+        };
+        // 将任务添加到延迟队列中
+        timer.newTimeout(task, 0, TimeUnit.SECONDS);
+
     }
 
     /**
