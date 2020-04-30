a05882c6c793995b6a2c327932bd394b7f82eadb
wanglei <295628068@qq.com>
2020-04-30 15:12:24 +0800

    完善 HashMap 循环和删除的代码

--- blog-example/src/main/java/com/example/HashMapCycle.java
+++ blog-example/src/main/java/com/example/HashMapCycle.java
@@ -16,58 +16,39 @@ public class HashMapCycle {
     }};
 
     public static void main(String[] args) {
-//        entrySet();
-//        keySet();
-//        forEachEntrySet();
-//        forEachKeySet();
-//        lambda();
-//        streamApi();
-//        parallelStreamApi();
+        entrySet();
+        keySet();
+        forEachEntrySet();
+        forEachKeySet();
+        lambda();
+        streamApi();
+        parallelStreamApi();
 
-//        // 安全的删除
-//        Iterator<Map.Entry<Integer, String>> iterator = map.entrySet().iterator();
-//        while (iterator.hasNext()) {
-//            Map.Entry<Integer, String> entry = iterator.next();
-//            if (entry.getKey() == 1) {
-//                // 删除
-//                System.out.println("del:" + entry.getKey());
-//                iterator.remove();
-//            } else {
-//                System.out.println("show:" + entry.getKey());
-//            }
-//        }
-
-
-        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+        // 安全的删除
+        Iterator<Map.Entry<Integer, String>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, String> entry = iterator.next();
             if (entry.getKey() == 1) {
                 // 删除
                 System.out.println("del:" + entry.getKey());
-                map.remove(entry.getKey());
+                iterator.remove();
             } else {
                 System.out.println("show:" + entry.getKey());
             }
         }
-//        show:0
-//        del:1
-//        Exception in thread "main" java.util.ConcurrentModificationException
-//        at java.util.HashMap$HashIterator.nextNode(HashMap.java:1437)
-//        at java.util.HashMap$EntryIterator.next(HashMap.java:1471)
-//        at java.util.HashMap$EntryIterator.next(HashMap.java:1469)
-//        at com.example.HashMapTest.main(HashMapTest.java:40)
 
+        // 根据 map 中的 key 去判断删除
+        map.keySet().removeIf(key -> key == 1);
+        map.forEach((key, value) -> {
+            if (key == 1) {
+                System.out.println("del:" + key);
+                map.remove(key);
+            } else {
+                System.out.println("show:" + key);
+            }
+        });
 
-//        // 根据 map 中的 key 去判断删除
-//        map.keySet().removeIf(key -> key == 1);
-//        map.forEach((key, value) -> {
-//            if (key == 1) {
-//                System.out.println("del:" + key);
-//                map.remove(key);
-//            } else {
-//                System.out.println("show:" + key);
-//            }
-//        });
-
-
+        // stream api filter
         map.entrySet().stream().filter(m -> 1 != m.getKey()).forEach((entry) -> {
             if (entry.getKey() == 1) {
                 System.out.println("del:" + entry.getKey());
@@ -75,16 +56,6 @@ public class HashMapCycle {
                 System.out.println("show:" + entry.getKey());
             }
         });
-
-//        map.entrySet().stream().forEach((entry) -> {
-//            if (entry.getKey() == 1) {
-//                System.out.println("del:" + entry.getKey());
-//                map.remove(entry.getKey());
-//            } else {
-//                System.out.println("show:" + entry.getKey());
-//            }
-//        });
-
     }
 
 
