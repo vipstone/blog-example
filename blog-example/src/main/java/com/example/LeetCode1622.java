106fc3d43d3cb1c6f2ddc9af634ada196a9aae00
wanglei <295628068@qq.com>
2020-11-23 21:27:09 +0800

    新增字符串数组对比实现代码

---
+++ blog-example/src/main/java/com/example/LeetCode1622.java
@@ -0,0 +1,29 @@
+package com.example;
+
+/**
+ * 1662. 检查两个字符串数组是否相等
+ * https://leetcode-cn.com/problems/check-if-two-string-arrays-are-equivalent/
+ */
+public class LeetCode1622 {
+    // 方法一:遍历将字符串整合之后对比
+    class Solution {
+        public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+            StringBuilder s1 = new StringBuilder();
+            StringBuilder s2 = new StringBuilder();
+            for (String i : word1) {
+                s1.append(i);
+            }
+            for (String i : word2) {
+                s2.append(i);
+            }
+            return s1.toString().equals(s2.toString());
+        }
+    }
+
+    // 方法二:使用内置方法合并字符串
+    class Solution2 {
+        public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+            return String.join("", word1).toString().equals(String.join("", word2));
+        }
+    }
+}
